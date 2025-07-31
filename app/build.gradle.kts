c2c23332fdf3fef3e4fbeed88f26da9b57ebdcbc
ladors1 <126808963+ladors1@users.noreply.github.com>
2025-08-01 01:25:33 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation(name = "colorpicker-0.0.13", ext = "aar")
+    implementation(files("libs/colorpicker-0.0.13.aar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
