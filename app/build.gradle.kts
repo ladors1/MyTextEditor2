6e12929d1a20afccac5a0656eeadc23873884002
ladors1 <126808963+ladors1@users.noreply.github.com>
2025-08-01 01:48:14 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("yuku.ambilwarna:ambilwarna:2.0.1") // فقط این ColorPicker استفاده می‌شود
+    implementation("com.github.yukuku:ambilwarna:2.0.1")  // ← آدرس درست AmbilWarna در JitPack
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
