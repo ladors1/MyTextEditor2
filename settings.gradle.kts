68f4f41ffe6d28929e943a44daf2a5d1b3eeebb7
ladors1 <126808963+ladors1@users.noreply.github.com>
2025-08-01 01:44:17 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,18 +3,14 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
-
 rootProject.name = "MyTextEditor2"
 include(":app")
