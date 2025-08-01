95a199aaa437fb0a5d88f4623685ecd9cf023396
ladors1 <126808963+ladors1@users.noreply.github.com>
2025-08-01 14:37:17 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,17 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    
+    // این بخش را برای استفاده از View Binding اضافه کنید که کار با UI را ساده‌تر می‌کند
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    // این خط برای طراحی جدید و مدرن برنامه ضروری است
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.github.skydoves:colorpickerview:2.2.4")
     testImplementation("junit:junit:4.13.2")
